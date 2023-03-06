44e5b52ee4edfa2e5134178ea405add0f458ecb6
annpersim <annasharonova767@gmail.com>
2023-03-07 00:02:13 +0300

    add components

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+export const FriendListItem = ({ friend }) => {
+  return (
+    <li className={css.item}>
+      {friend.isOnline == true ? (
+        <span className={css['status-online']}></span>
+      ) : (
+        <span className={css['status-offline']}></span>
+      )}
+      <img
+        className={css.avatar}
+        src={friend.avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className="name">{friend.name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+};
