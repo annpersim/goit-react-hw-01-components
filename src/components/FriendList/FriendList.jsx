823e7c72a4d7f7e3b7ed121a0d664271130ad221
annpersim <annasharonova767@gmail.com>
2023-03-07 23:31:06 +0300

    fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,25 @@
-import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
+const FriendListItem = ({ friend }) => {
+  const statusStyle = friend.isOnline ? css.online : css.offline;
+  return (
+    <li className={css.item}>
+      <span className={statusStyle}></span>
+      <img
+        className={css.avatar}
+        src={friend.avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className="name">{friend.name}</p>
+    </li>
+  );
+};
+
 export const FriendList = ({ friends }) => {
   return (
-    <ul className={css['friend-list']}>
+    <ul className={css.friendList}>
       {friends.map(friend => (
         <FriendListItem key={friend.id} friend={friend} />
       ))}
@@ -18,4 +33,12 @@ FriendList.prototypes = {
       id: PropTypes.number.isRequired,
     })
   ).isRequired,
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className={css['transaction-history']}>
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
           <th>Type</th>
